95140a56e5a0366ee02a18f7e683caf30b802f08
kingjack05 <kingjack05@gmail.com>
2022-12-31 15:22:40 +0800

    init

--- next.config.js
+++ next.config.js
@@ -1,15 +1,27 @@
-require('dotenv').config();
-const { sizes } = require('./blocks/Image/sizes');
+require('dotenv').config()
+const { sizes } = require('./blocks/Image/sizes')
 
 module.exports = {
-  publicRuntimeConfig: {
-    SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
-  },
-  images: {
-    domains: [
-      'localhost',
-      // Your domain(s) here
-    ],
-    deviceSizes: sizes,
-  },
-};
+    publicRuntimeConfig: {
+        SERVER_URL: process.env.PAYLOAD_PUBLIC_SERVER_URL,
+    },
+    images: {
+        domains: [
+            'localhost',
+            // Your domain(s) here
+        ],
+        deviceSizes: sizes,
+    },
+    webpack: (
+        config,
+        { buildId, dev, isServer, defaultLoaders, nextRuntime, webpack },
+    ) => {
+        // Important: return the modified config
+        return config
+    },
+    eslint: {
+        // Warning: This allows production builds to successfully complete even if
+        // your project has ESLint errors.
+        ignoreDuringBuilds: true,
+    },
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
-import { GetServerSideProps } from 'next';
-import Page, { getServerSideProps as sharedGetServerSideProps } from './[...slug]';
+import { GetServerSideProps } from 'next'
+import Page, {
+    getServerSideProps as sharedGetServerSideProps,
+} from './[...slug]'
 
-export default Page;
+export default () => <>Hello</>
 
-export const getServerSideProps: GetServerSideProps = async (ctx) => {
-  const func = sharedGetServerSideProps.bind(this);
-  return func(ctx);
-};
+export const getServerSideProps: GetServerSideProps = async ctx => {
+    const func = sharedGetServerSideProps.bind(this)
+    return func(ctx)
+}
