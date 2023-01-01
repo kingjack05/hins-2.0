3701359191712e20ba3fbbfcb88abfb8a7697cbc
kingjack05 <kingjack05@gmail.com>
2023-01-01 23:50:39 +0800

    feature: table of content

---
+++ components/TableOfContent/index.tsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from 'react'
+import styles from './index.module.css'
+
+const getClassName = level => {
+    switch (level) {
+        case 2:
+            return 'head2'
+        case 3:
+            return 'head3'
+        case 4:
+            return 'head4'
+        default:
+            return null
+    }
+}
+
+function TableOfContent() {
+    const [headings, setHeadings] = useState([])
+    useEffect(() => {
+        const elements = Array.from(
+            document.querySelectorAll('h2, h3, h4'),
+        ).map(elem => ({
+            id: elem.id,
+            text: elem.textContent,
+            level: Number(elem.nodeName.charAt(1)),
+        }))
+        setHeadings(elements)
+    }, [])
+
+    return (
+        <nav className={styles.toc}>
+            <ul>
+                {headings.map(heading => (
+                    <li
+                        key={heading.id}
+                        className={styles[getClassName(heading.level)]}
+                    >
+                        <a
+                            href={`#${heading.id}`}
+                            className={styles.tocItem}
+                            onClick={e => {
+                                e.preventDefault()
+                                document
+                                    .querySelector(`#${heading.id}`)
+                                    .scrollIntoView({
+                                        behavior: 'smooth',
+                                    })
+                            }}
+                        >
+                            {heading.text}
+                        </a>
+                    </li>
+                ))}
+            </ul>
+        </nav>
+    )
+}
+
+export default TableOfContent
--- pages/diagnosis/[slug].tsx
+++ pages/diagnosis/[slug].tsx
@@ -2,34 +2,41 @@ import React from 'react'
 import payload from 'payload'
 import { getDiagnosis } from '../../api'
 import RichText from '../../components/RichText'
+import TableOfContent from '../../components/TableOfContent'
+import Head from '../../components/Head'
+import classes from '../../css/diagnosis.module.css'
 
 const DiagnosisRenderer = ({ slug, item }) => {
     // return <pre>{JSON.stringify(item, null, 2)}</pre>
     return (
         <>
-            <section>
-                <h2>Admission Note</h2>
-                <RichText content={item['admission_note']} />
-                <h2>Progress Note</h2>
-                <b>Subjective</b>
-                <RichText content={item['progress_note']['subjective']} />
-                <b>Objective</b>
-                <RichText content={item['progress_note']['objective']} />
-                <b>Assessment</b>
-                <RichText content={item['progress_note']['assessment']} />
-                <b>Plan</b>
-                <RichText content={item['progress_note']['plan']} />
-                <h2>Labs</h2>
-                <RichText content={item['lab']} />
-                <h2>Physical Examination</h2>
-                <RichText content={item['physical_examintaion']} />
-                <h2>Plan</h2>
-                <RichText content={item['plan']} />
-                <h2>Risk Factors</h2>
-                <RichText content={item['risk_factors']} />
-                <h2>Summary Note</h2>
-                <RichText content={item['summary_note']} />
-            </section>
+            <Head title={item.name} />
+            <div className={classes.wrapper}>
+                <section className={classes.content}>
+                    <h2 id="admission">Admission Note</h2>
+                    <RichText content={item['admission_note']} />
+                    <h2 id="progress">Progress Note</h2>
+                    <b>Subjective</b>
+                    <RichText content={item['progress_note']['subjective']} />
+                    <b>Objective</b>
+                    <RichText content={item['progress_note']['objective']} />
+                    <b>Assessment</b>
+                    <RichText content={item['progress_note']['assessment']} />
+                    <b>Plan</b>
+                    <RichText content={item['progress_note']['plan']} />
+                    <h2 id="labs">Labs</h2>
+                    <RichText content={item['lab']} />
+                    <h2 id="PE">Physical Examination</h2>
+                    <RichText content={item['physical_examintaion']} />
+                    <h2 id="plan">Plan</h2>
+                    <RichText content={item['plan']} />
+                    <h2 id="risk_factors">Risk Factors</h2>
+                    <RichText content={item['risk_factors']} />
+                    <h2 id="summary">Summary Note</h2>
+                    <RichText content={item['summary_note']} />
+                </section>
+                <TableOfContent />
+            </div>
         </>
     )
 }
